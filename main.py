06770b19fcdfeeabd77b760f49adb8be1687020a
AlexGolovko <golovkobalak@gmail.com>
2020-02-17 22:34:33 +0200

    add and test smoke and fire checker

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import machine
 from machine import Pin, I2C
 import dht
 
+
 def measureDHT11():
     sensor = dht.DHT11(Pin(22))
     sensor.measure()
     temp = sensor.temperature()
     hum = sensor.humidity()
+    measure = [temp, hum]
+    return measure
+
 
 def relay():
-   pin = Pin(0,Pin.OUT)
-   pin.on()
-   utime.sleep(3)
-   pin.off()
+    pin = Pin(0, Pin.OUT)
+    pin.on()
+    utime.sleep(3)
+    pin.off()
 
 
 def do_connect():
@@ -27,17 +31,22 @@ def do_connect():
             pass
     print('network config:', wlan.ifconfig())
 
+def  measureFire():
+    fire = machine.ADC(Pin(35))
+    return fire.read()
+
+def measureSmoke():
+    smoke =machine.ADC(Pin(32))
+    return smoke.read()
+
+
 def main():
-    d = dht12.DHT11(machine.Pin(22))
-    led = Pin(2, Pin.OUT)
-    enabled = False
-    while True:
-        if enabled:
-            led.off()
-        else:
-            led.on()
-        utime.sleep_ms(1000)
-        enabled = not enabled
+    do_connect()
+    measures = measureDHT11()
+    fire = measureFire()
+    measures.append(fire)
+
+
 
 if __name__ == '__main__':
     main()
