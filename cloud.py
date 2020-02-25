85cc3c1c52723ba84708730c5e3986a0c8316d36
Alex <golovkobalak@gmail.com>
2020-02-25 17:44:45 +0200

    refactored in class

--- cloud.py
+++ cloud.py
@@ -2,33 +2,35 @@ from umqtt.simple import MQTTClient
 import properties as prop
 
 # init program
-MQTT_SERVER = prop.server()
-MQTT_PORT = prop.port()
-MQTT_USER = prop.user()
-MQTT_PASS = prop.user_pass()
-MQTT_TIMEOUT = 30
-MQTT_CLIENT = "esp32"
 
-topic_l1 = 'sensors/'
+class Cloud:
+    
+    MQTT_SERVER = prop.server()
+    MQTT_PORT = prop.port()
+    MQTT_USER = prop.user()
+    MQTT_PASS = prop.user_pass()
+    MQTT_TIMEOUT = 30
+    MQTT_CLIENT = "esp32"
+    topic_l1 = 'sensors/'
 
-
-
-def setTopic(topic):
-    topic_l1 = topic
-
+    def setTopic(topic):
+        topic_l1 = topic
+    def __init__(self):
+        self.client = client()
 
 # helper methods
-def mqtt_publish(topic_l2, payload, retain=False, qos=0):
-    client = MQTTClient(client_id=MQTT_CLIENT, server=MQTT_SERVER, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASS)
-    client.connect()
-    client.publish(topic_l1 + topic_l2, payload, retain, 0)
-    print('mqtt published msg: %s; to topic: %s' % (payload, topic_l1 + topic_l2))
+    def mqtt_publish(self,topic_l2, payload, retain=False, qos=0):
+        self.client.connect()
+        self.client.publish(topic_l1 + topic_l2, payload, retain, 0)
+        self.client.disconnect()
+        print('mqtt published msg: %s; to topic: %s' % (payload, topic_l1 + topic_l2))
 
 
-def sub_cb(topic, msg):
-    print('got msg')
+    def sub_cb(topic, msg):
+        print('got msg')
 
 
-def client():
-    # client.set_last_will('status', 'disconnected', retain=True)
-    return client
+    def client(self):
+        self.client = MQTTClient(client_id=MQTT_CLIENT, server=MQTT_SERVER, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASS)
+        # client.set_last_will('status', 'disconnected', retain=True)
+        return client
