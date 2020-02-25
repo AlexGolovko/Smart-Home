c21b05721fd9abf3ca55fc3506d89afb7ef7289b
Alex <golovkobalak@gmail.com>
2020-02-25 17:30:31 +0200

    removed do_connect()

--- main.py
+++ main.py
@@ -29,19 +29,6 @@ def relay():
     utime.sleep(3)
     pin.off()
 
-
-def do_connect():
-    import network
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect('royter', 'traveller22')
-        while not wlan.isconnected():
-            pass
-    print('network config:', wlan.ifconfig())
-
-
 def measureFire():
     fire = machine.ADC(Pin(35))
     return 4095 - fire.read()
@@ -51,14 +38,14 @@ def measureSmoke():
     smoke = machine.ADC(Pin(32))
     return smoke.read()
 
+def collectData():
+    return measureDHT11(), measureFire(), measureSmoke()
 
 def main():
     # client = cloud.client()
     while True:
         names = ['temperature', 'humidity', 'fire', 'smoke']
-        measures = measureDHT11()
-        measures.append(measureFire())
-        measures.append(measureSmoke())
+        measures = collectData()      
         print('temp= ' + str(measures[0]) + ' hum= ' + str(measures[1]) + ' fire = ' + str(
             measures[2]) + ' smoke = ' + str(measures[3]))
         for idx in range(4):
