f2f4af2270c7b7ec80e61569f3473c9c47e314a3
AlexGolovko <golovkobalak@gmail.com>
2020-04-17 10:35:46 +0300

    Files tree fixed

--- SmartHomeBot/src/main/java/com/golovkobalak/smarthomebot/bot/Bot.java
+++
@@ -1,9 +0,0 @@
-package com.golovkobalak.smarthomebot.bot;
-
-import com.pengrad.telegrambot.UpdatesListener;
-
-public interface Bot<T> {
-    void  sendAlarmMessage(T t);
-    void setUpdateHandler(UpdatesListener listener);
-
-}
