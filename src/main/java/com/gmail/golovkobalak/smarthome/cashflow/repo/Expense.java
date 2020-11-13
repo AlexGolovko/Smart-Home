13d9dd740f4fb12557d51aa2a960f88f8e8c2128
AlexGolovko <golovkobalak@gmail.com>
2020-11-13 17:36:31 +0200

    fire the build

--- src/main/java/com/gmail/golovkobalak/smarthome/cashflow/repo/Expense.java
+++ src/main/java/com/gmail/golovkobalak/smarthome/cashflow/repo/Expense.java
@@ -3,8 +3,6 @@ package com.gmail.golovkobalak.smarthome.cashflow.repo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-
 @Document
 public class Expense {
     @Id
