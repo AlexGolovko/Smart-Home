ceee18df4a4ecd63f521d5298b9bc328ac3b0050
AlexGolovko <golovkobalak@gmail.com>
2020-11-16 16:55:05 +0200

    Fix github actions build

--- IOT ESP32 python/cloud.py
+++ IOT ESP32 python/cloud.py
@@ -5,13 +5,13 @@ import properties as prop
 # init program
 
 class Cloud:
-    MQTT_SERVER = prop.server()
-    MQTT_PORT = prop.port()
-    MQTT_USER = prop.user()
-    MQTT_PASS = prop.user_pass()
+    MQTT_SERVER = '192.168.31.16'
+    MQTT_PORT = '1883'
+    # MQTT_USER =
+    # MQTT_PASS =
     MQTT_TIMEOUT = 30
-    MQTT_CLIENT = "esp32"
-    topic_l1 = 'sensors/'
+    MQTT_CLIENT = "kitchen"
+    topic_l1 = 'sensors/home'
 
     def setTopic(self, topic):
         self.topic_l1 = topic
@@ -20,9 +20,11 @@ class Cloud:
         self.client = self.client()
 
     # helper methods
-    def mqtt_publish(self, topic_l2, payload, retain=False, qos=0):
+    # def mqtt_publish(self, topic_l2, payload, retain=False, qos=0):
+    def mqtt_publish(self, payload, retain=False, qos=0, topic_l2=''):
         self.client.connect()
         self.client.publish(self.topic_l1 + topic_l2, payload, retain, 0)
+        self.client.publish(payload, retain, 0)
         self.client.disconnect()
         print('mqtt published msg: %s; to topic: %s' % (payload, self.topic_l1 + topic_l2))
 
@@ -30,7 +32,8 @@ class Cloud:
         print('got msg')
 
     def client(self):
-        self.client = MQTTClient(client_id=self.MQTT_CLIENT, server=self.MQTT_SERVER, port=self.MQTT_PORT,
-                                 user=self.MQTT_USER, password=self.MQTT_PASS)
+        # self.client = MQTTClient(client_id=self.MQTT_CLIENT, server=self.MQTT_SERVER, port=self.MQTT_PORT,
+        #                          user=self.MQTT_USER, password=self.MQTT_PASS)
+        self.client = MQTTClient(client_id=self.MQTT_CLIENT, server=self.MQTT_SERVER, port=self.MQTT_PORT)
         # client.set_last_will('status', 'disconnected', retain=True)
         return self.client
--- IOT ESP32 python/umqtt/simple.py
+++ IOT ESP32 python/umqtt/simple.py
@@ -44,39 +44,7 @@ class MQTTClient:
     def set_callback(self, f):
         self.cb = f
 
-    def s# This file is executed on every boot (including wake-boot from deepsleep)
-# import esp
-# esp.osdebug(None)
-# import gc
-# import webrepl
-# webrepl.start()
-import network
-import machine
-import esp
-
-machine.freq(240000000)
-esp.osdebug(None)
-
-
-def do_connect():
-    import properties
-    import uping
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(properties.ssid(), properties.password())
-        while not wlan.isconnected():
-            pass
-    print('network config:', wlan.ifconfig())
-    print(uping.ping('google.com'))
-
-
-do_connect()
-import gc
-gc.collect()
-print('wifi connected')
-et_last_will(self, topic, msg, retain=False, qos=0):
+    def set_last_will(self, topic, msg, retain=False, qos=0):
         assert 0 <= qos <= 2
         assert topic
         self.lw_topic = topic
