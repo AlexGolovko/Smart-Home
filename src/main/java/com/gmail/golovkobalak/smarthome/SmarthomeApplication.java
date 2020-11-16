78fbd84bdea727acc854abe8c29d2d0bd73b6f5f
AlexGolovko <golovkobalak@gmail.com>
2020-11-16 15:38:53 +0200

    Decrease memory usage be smarthome container

--- src/main/java/com/gmail/golovkobalak/smarthome/SmarthomeApplication.java
+++ src/main/java/com/gmail/golovkobalak/smarthome/SmarthomeApplication.java
@@ -1,15 +1,21 @@
 package com.gmail.golovkobalak.smarthome;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class SmarthomeApplication {
 
 
     public static void main(String[] args) {
         final ConfigurableApplicationContext context = SpringApplication.run(SmarthomeApplication.class, args);
+        final Runtime runtime = Runtime.getRuntime();
+        log.info("TotalMemory,mb:" + runtime.totalMemory()*0.000001);
+        log.info("FreeMemory,mb:" + runtime.freeMemory()*0.000001);
+        log.info("MaxMemory,mb:" + runtime.maxMemory()*0.000001);
         final Runner runner = context.getBean(Runner.class);
         runner.run();
     }
